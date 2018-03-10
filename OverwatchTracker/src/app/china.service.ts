5303e38aa21a01785925ccecd3951163e4864eae
Sairen <sairen@gmail.com>
2018-03-10 22:00:34 +0200

    fixing prices

--- OverwatchTracker/src/app/china.service.ts
+++ OverwatchTracker/src/app/china.service.ts
@@ -78,37 +78,37 @@ initEveryting()
             new GameItem("Как снег на ветру", "anavoice1", 25),
             new GameItem("Я сама творю свою удачу", "anavoice2", 75),
             new GameItem("Танец", "anaspray1", 25),
-            new GameItem("Танец дракона", "anaspray2", 75)));
+            new GameItem("Танец дракона", "anaspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Бастион",
             new GameItem("Вуп-дуу-вуу", "basvoice1", 25),
             new GameItem("Ду-у ви-и ду-у ви-и ву-у", "basvoice2", 75),
             new GameItem("Вкусняшкобот", "basspray1", 25),
-            new GameItem("Танец дракона", "basspray2", 75)));
+            new GameItem("Танец дракона", "basspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Вдова",
             new GameItem("О, благодарю", "widvoice1", 25),
             new GameItem("Никакой породы", "widvoice2", 75),
             new GameItem("Заколка", "widspray1", 25),
-            new GameItem("Танец дракона", "widspray2", 75)));
+            new GameItem("Танец дракона", "widspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Генджи",
             new GameItem("Познать себя", "genvoice1", 25),
             new GameItem("Собака лает - караван идет", "genvoice2", 75),
             new GameItem("Зеленый дракон", "genspray1", 25),
-            new GameItem("Танец дракона", "genspray2", 75)));
+            new GameItem("Танец дракона", "genspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Дзенъятта",
             new GameItem("Каждый хвалит свой насест", "zenvoice1", 25),
             new GameItem("Удача улыбается мне", "zenvoice2", 75),
             new GameItem("Ют Нори", "zenspray1", 25),
-            new GameItem("Танец дракона", "zenspray2", 75)));
+            new GameItem("Танец дракона", "zenspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Дива",
             new GameItem("Радости жизни", "divavoice1", 25),
             new GameItem("С новым годом!", "divavoice2", 75),
-            new GameItem("Качели", "divaspray1", 75),
-            new GameItem("Танец дракона", "divaspray2", 75)));
+            new GameItem("Качели", "divaspray1", 25),
+            new GameItem("Танец дракона", "divaspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Думфист",
             new GameItem("Подачки ждешь?", "doovoice1", 75),
@@ -120,37 +120,37 @@ initEveryting()
             new GameItem("Вот это феерверк", "reavoice1", 25),
             new GameItem("Я везучь", "reavoice2", 75),
             new GameItem("Огненный цветок", "reaspray1", 25),
-            new GameItem("Танец дракона", "reaspray2", 75)));
+            new GameItem("Танец дракона", "reaspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Заря",
             new GameItem("Прокачайся", "zarvoice1", 25),
             new GameItem("Вот где собака зарыта", "zarvoice2", 75),
             new GameItem("На массе", "zarspray1", 25),
-            new GameItem("Танец дракона", "zarspray2", 75)));
+            new GameItem("Танец дракона", "zarspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Крысавчик",
             new GameItem("Си фа цай!", "junvoice1", 25),
             new GameItem("Хоть волком вой", "junvoice2", 75),
             new GameItem("Я лечу!", "junspray1", 25),
-            new GameItem("Танец дракона", "junspray2", 75)));
+            new GameItem("Танец дракона", "junspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Лусио",
             new GameItem("Со мной все становится лучше!", "lucvoice1", 25),
             new GameItem("Это я люблю!", "lucvoice2", 75),
             new GameItem("Давай на счет", "lucspray1", 25),
-            new GameItem("Танец дракона", "lucspray2", 75)));
+            new GameItem("Танец дракона", "lucspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("МакКри",
             new GameItem("Это надо отметить", "mccvoice1", 25),
             new GameItem("Как пес бездомный", "mccvoice2", 75),
             new GameItem("Орел или решка?", "mccspray1", 25),
-            new GameItem("Танец дракона", "mccspray2", 75)));
+            new GameItem("Танец дракона", "mccspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Мерси",
             new GameItem("Впечатляет", "mervoice1", 25),
             new GameItem("Как же тебе повезло!", "mervoice2", 75),
             new GameItem("Зонтик", "merspray1", 25),
-            new GameItem("Танец дракона", "merspray2", 75)));
+            new GameItem("Танец дракона", "merspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Мойра",
             new GameItem("Ваше здоровье!", "moivoice1", 75),
@@ -162,7 +162,7 @@ initEveryting()
             new GameItem("Счастья и процветания!", "meivoice1", 25),
             new GameItem("Где мой красный конверт?", "meivoice2", 75),
             new GameItem("Удача", "meispray1", 25),
-            new GameItem("Танец дракона", "meispray2", 75)));
+            new GameItem("Танец дракона", "meispray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Ориса",
             new GameItem("Без Луны звезды светят ярче", "orivoice1", 75),
@@ -174,61 +174,61 @@ initEveryting()
             new GameItem("Ты что ж это цыпленок?", "reivoice1", 25),
             new GameItem("Судьба благоволит бесстрашным", "reivoice2", 75),
             new GameItem("Танец льва", "reispray1", 25),
-            new GameItem("Танец дракона", "reispray2", 75)));
+            new GameItem("Танец дракона", "reispray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Симметра",
             new GameItem("Какая удача", "symvoice1", 25),
             new GameItem("Повод отметить", "symvoice2", 75),
             new GameItem("Фонарь", "symspray1", 25),
-            new GameItem("Танец дракона", "symspray2", 75)));
+            new GameItem("Танец дракона", "symspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Солдат76",
             new GameItem("Петухи запели в полночь", "solvoice1", 25),
             new GameItem("Уважайте старших", "solvoice2", 75),
             new GameItem("Сложенные руки", "solspray1", 25),
-            new GameItem("Танец дракона", "solspray2", 75)));
+            new GameItem("Танец дракона", "solspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Сомбра",
             new GameItem("Вас так легко удивить", "somvoice1", 25),
             new GameItem("Собака бывает кусачей", "somvoice2", 75),
             new GameItem("Гадание", "somspray1", 25),
-            new GameItem("Танец дракона", "somspray2", 75)));
+            new GameItem("Танец дракона", "somspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Торбьорн",
             new GameItem("Что, горячо?", "torvoice1", 25),
             new GameItem("А ну все сюда!", "torvoice2", 75),
             new GameItem("Золото", "torspray1", 25),
-            new GameItem("Танец дракона", "torspray2", 75)));
+            new GameItem("Танец дракона", "torspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Трейсер",
             new GameItem("Все готовы устроить салют?", "travoice1", 25),
             new GameItem("На луну и обратно", "travoice2", 75),
             new GameItem("Танец с веерами", "traspray1", 25),
-            new GameItem("Танец дракона", "traspray2", 75)));
+            new GameItem("Танец дракона", "traspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Турбосвин",
             new GameItem("Ты кура?", "roavoice1", 25),
             new GameItem("Ну и пес с ним", "roavoice2", 75),
             new GameItem("Баоцзы", "roaspray1", 25),
-            new GameItem("Танец дракона", "roaspray2", 75)));
+            new GameItem("Танец дракона", "roaspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Уинстон",
             new GameItem("Вот это круто!", "winvoice1", 25),
             new GameItem("Луна, мой родной дом", "winvoice2", 75),
             new GameItem("Воздушный змей", "winspray1", 25),
-            new GameItem("Танец дракона", "winspray2", 75)));
+            new GameItem("Танец дракона", "winspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Фара",
             new GameItem("Я обеспечу мир", "phavoice1", 25),
             new GameItem("Устроить салют?", "phavoice2", 75),
             new GameItem("С Новым годом!", "phaspray1", 25),
-            new GameItem("Танец дракона", "phaspray2", 75)));
+            new GameItem("Танец дракона", "phaspray2", 25)));
 
         this.completeInfo.allHeroes.push(new Hero("Ханзо",
             new GameItem("День полной луны", "hanvoice1", 25),
             new GameItem("Наглый щенок!", "hanvoice2", 75),
             new GameItem("Синий дракон", "hanspray1", 25),
-            new GameItem("Танец дракона", "hanspray2", 75)));
+            new GameItem("Танец дракона", "hanspray2", 25)));
 
         this.saveInfo();
     }
